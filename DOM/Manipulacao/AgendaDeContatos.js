8337511c896c94dd7cac76ebd246e94e18eeed02
VianaSamuel <samuluizzz@hotmail.com>
2023-05-23 08:30:21 -0300

    Alterações pequenas.

--- DOM/Manipulacao/AgendaDeContatos.js
+++ DOM/Manipulacao/AgendaDeContatos.js
@@ -2,40 +2,47 @@ function addContact(){
     // puxa a seção (section) de contatos
 	const contactSection = document.getElementById('contacts-list')
 
-    // cria o título (h3) e a lista (ul)
+    // cria o título (h3)
     const h3 = document.createElement('h3')
     h3.innerText = 'Contato'
+    // cria a lista (ul)
     const ul = document.createElement('ul')
 
-    // cria o item (li) nome e o amarra na lista (ul)
+    // cria o ITEM (li) nome
     const nameLi = document.createElement('li')
     nameLi.innerText = 'Nome: '
+    // cria o INPUT
     const nameInput = document.createElement('input')
     nameInput.type = 'text'
     nameInput.name = 'fullname'
-    // amarra o nome (input) no item (li)
+    //
+    // amarra o input no item (li)
     nameLi.appendChild(nameInput)
     // amarra o item (li) na lista (ul)
     ul.append(nameLi, document.createElement('br'))
     
-    // cria o item (li) telefone e o amarra na lista (ul)
+    // cria o ITEM (li) telefone
     const phoneLi = document.createElement('li')
     phoneLi.innerText = 'Telefone: '
+    // cria o INPUT
     const phoneInput = document.createElement('input')
     phoneInput.type = 'text'
     phoneInput.name = 'phone'
-    // amarra o telefone (input) no item (li)
+    //
+    // amarra o input no item (li)
     phoneLi.appendChild(phoneInput)
     // amarra o item (li) na lista (ul)
     ul.append(phoneLi, document.createElement('br'))
     
-    // cria o item (li) endereço e o amarra na lista (ul)
+    // cria o ITEM (li) endereço
     const addressLi = document.createElement('li')
     addressLi.innerText = 'Endereço: '
+    // cria o INPUT
     const addressInput = document.createElement('input')
     addressInput.type = 'text'
     addressInput.name = 'address'
-    // amarra o endereço (input) no item (li)
+    //
+    // amarra o input no item (li)
     addressLi.appendChild(addressInput)
     // amarra o item (li) na lista (ul)
     ul.append(addressLi, document.createElement('br'))
